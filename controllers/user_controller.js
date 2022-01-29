fad727a00b0757b8ea3c4b2ed5780042192ffe19
Vuyisile <vtlehola23@gmail.com>
2022-01-29 20:03:06 +0200

    add basic user info to database

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -5,7 +5,19 @@ module.exports = {
          res.render('register.ejs');
     },
     create_user: (req, res, next) => {
-        
+        let user_params = {
+            email: req.body.email,
+            name: req.body.name,
+            master_password: req.body.master_password,
+            master_password_hint: req.body.master_password_hint
+        }
+        User.create(user_params).then(user => {
+            res.locals.redirect = '/';
+            console.log(`User ${user.name} successfully created`);
+            next();
+        }).catch(error => {
+            console.log(`${error.message}`);
+        })
     },
     edit_user: (req, res) => {
         
@@ -17,6 +29,8 @@ module.exports = {
         
     },
     redirect_user_view: (req, res) => {
-        
+        let redirect_path = res.locals.redirect;
+        if(redirect_path) res.redirect(redirect_path);
+        else next();
     }
 }
