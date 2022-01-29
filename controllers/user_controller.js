d0f4d075e67a18dd02e484094ec72beb90aca88e
Vuyisile <vtlehola23@gmail.com>
2022-01-29 19:30:31 +0200

    initial commit

---
+++ controllers/user_controller.js
@@ -0,0 +1,22 @@
+const User = require('../models/user');
+
+module.exports = {
+    new_user: (req, res) => {
+         res.render('register.ejs');
+    },
+    create_user: (req, res, next) => {
+        
+    },
+    edit_user: (req, res) => {
+        
+    },
+    update_user: (req, res) => {
+        
+    },
+    delete_user: (req, res) => {
+        
+    },
+    redirect_user_view: (req, res) => {
+        
+    }
+}
