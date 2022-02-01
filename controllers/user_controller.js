ca7c89bb091240180cdad9fb4319b78b73763b61
Vuyisile <vtlehola23@gmail.com>
2022-02-01 12:56:08 +0200

    prevent app from saving empty form fields to database

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -4,31 +4,38 @@ const { validationResult } = require('express-validator');
 
 module.exports = {
     new_user: (req, res) => {
-         res.render('register.ejs', { error: req.flash('error')});
+         res.render('register', { error: req.flash('error'), 
+         validation_errors: req.flash('validation_errors')});
     },
     create_user: (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-          };
-
         let user_params = {
             email: req.body.email,
             name: req.body.name,
             master_password: req.body.master_password,
+            confirm_master_password: req.body.confirm_master_password,
             master_password_hint: req.body.master_password_hint
         }
-        User.create(user_params).then(user => {
-            req.flash('success', `${user.name}'s successfully created`);
-            res.locals.redirect = '/';
-            console.log(`User ${user.name} successfully created`);
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            let messages = errors.array().map(e => e.msg);
+            req.skip = true;
+            req.flash('validation_errors', messages);
+            res.locals.redirect = '/user/new';
             next();
-        }).catch(error => {
-            req.flash('error', `Failed to create user account because ${error.message}`);
-            res.locals.redirect = '/user/new'
-            console.log(`Error saving user: ${error.message}`);
-            
-        })
+            //return res.status(400).json({ errors: errors.array() });
+          }else{
+            User.create(user_params).then(user => {
+                req.flash('success', `${user.name} successfully created`);
+                res.locals.redirect = '/';
+                console.log(`User ${user.name} successfully created`);
+                next();
+            }).catch(error => {
+                req.flash('error', `Failed to create user account because ${error.message}`);
+                res.locals.redirect = '/user/new'
+                console.log(`Error saving user: ${error.message}`);
+                next();
+            })
+          }
     },
     edit_user: (req, res) => {
         
@@ -39,7 +46,7 @@ module.exports = {
     delete_user: (req, res) => {
         
     },
-    redirect_user_view: (req, res) => {
+    redirect_user_view: (req, res, next) => {
         let redirect_path = res.locals.redirect;
         if(redirect_path) res.redirect(redirect_path);
         else next();
