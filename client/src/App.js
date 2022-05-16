198596d1f10fd9fac9f5a8f28621ad58d3249995
Vuyisile <vtlehola23@gmail.com>
2022-05-16 15:06:27 +0200

    add basic routing between components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router";
+import './tailwind_output.css';
+import Home from './components/Home';
+import Register from './components/Register';
+import PasswordHint from './components/PasswordHint';
+import Vault from "./components/Vault";
 
 const App = () =>{
     return (
-        <h1>
-            Hello world! I am using React
-        </h1>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="register" element={<Register />} />
+          <Route path="passwordHint" element={<PasswordHint />} />
+          <Route path="vault" element={<Vault />} />
+        </Routes>
+      </div>
     )
 }
 
-export default App
+export default App;
