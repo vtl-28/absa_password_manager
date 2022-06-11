6b5dd576f43aca79e2e897711698c5f730cd7e92
Vuyisile <vtlehola23@gmail.com>
2022-06-11 15:00:12 +0200

    authenticate and login user

--- server/routes/home.js
+++ server/routes/home.js
@@ -13,6 +13,7 @@ app.use(passport.session());
 function is_auth(req, res, next){
     if(req.isAuthenticated()){
         res.locals.authenticated_user = req.user;
+        res.status(200).send("User loggin in")
         next();
     } else{
         res.status(401).json({ msg: 'You are not authorized to view this resource'});
@@ -22,11 +23,26 @@ function is_auth(req, res, next){
 //route to access home page
 app.get('/', index);
 //route authenticate and login registered user
-app.post('/login', passport.authenticate('local', {
-    failureRedirect: '/',
-    successRedirect: '/vault_landing_page',
-    failureFlash: true
-}));
+// app.post('/login', passport.authenticate('local', {
+//     failureRedirect: '/',
+//     successRedirect: '/vault_landing_page',
+//     failureFlash: true
+// }));
+app.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if(err) throw err;
+        if(!user) res.status(404).send("User does not exist");
+        else {
+            req.logIn(user, (err) => {
+              if (err) throw err;
+              res.status(200).send("Successfully Authenticated");
+              console.log(req.user);
+              console.log(req.session);
+              next();
+            });
+          }
+    })(req, res, next)
+});
 //route to access vault of authenticated user
 app.get('/vault_landing_page', is_auth, vault);
 //route to logout authenticated user
