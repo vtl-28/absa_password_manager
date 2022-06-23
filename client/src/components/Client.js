a585a74ac719f18410b30d0b9d80f22cf2aadf11
Vuyisile <vtlehola23@gmail.com>
2022-06-23 21:49:16 +0200

    initial commit

---
+++ client/src/components/Client.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export default function Client(){
+    return (
+        <div className="flex flex-col mt-2">
+            <h3 className="mb-2">There are no items to list</h3>
+            <button className="w-24 p-1 text-sm font-semibold text-red-600 border bg-gray-50 hover:text-white hover:bg-red-700 sm:w-36 sm:text-base" type="button">
+                <i className="fa-solid fa-plus"></i>Add item
+            </button>
+        </div>
+    )
+}
