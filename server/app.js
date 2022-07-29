f85697ead8ec759b4cadb4e7bbc9407d38594a02
Vuyisile <vtlehola23@gmail.com>
2022-07-29 09:21:10 +0200

    format and remove unused code

--- server/app.js
+++ server/app.js
@@ -1,71 +1,73 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const path = require('path');
-const favicon = require('serve-favicon');
-const mongoose = require('mongoose');
-const express_session = require('express-session');
-const cookie_parser = require('cookie-parser');
-const connect_flash = require('connect-flash');
-const method_override = require('method-override');
-const passport = require('passport');
-const MongoStore = require('connect-mongo');
-const cors = require('cors');
+const path = require("path");
+const favicon = require("serve-favicon");
+const mongoose = require("mongoose");
+const expressSession = require("express-session");
+const connectFlash = require("connect-flash");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const MongoStore = require("connect-mongo");
+const cors = require("cors");
 
+const connectDB = require("./config/db_conn");
+const homeRoute = require("./routes/home");
+const userRoute = require("./routes/user");
+const passwordHintRoute = require("./routes/password_hint");
+const applicationPasswordsRoute = require("./routes/application_passwords");
 
-const connect_db = require('./config/db_conn');
-const home_route = require('./routes/home');
-const user_route = require('./routes/user');
-const password_hint_route = require('./routes/password_hint');
-const application_passwords_route = require('./routes/application_passwords');
+connectDB();
 
-connect_db();
-
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(express.static('public'));
-app.use(express.static('assets'));
-app.use(express.static('node_modules'));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.use(express.static("public"));
+app.use(express.static("assets"));
+app.use(express.static("node_modules"));
 //app.set("view engine", "ejs");
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
-app.use(express.urlencoded({
-    extended:true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.set("port", process.env.PORT || 3000);
-app.use(connect_flash());
+app.use(connectFlash());
 
 app.use(
-    method_override("_method", {
-      methods: ["POST", "GET"]
-    })
-  );
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
 
-app.use(express_session({
+app.use(
+  expressSession({
     secret: process.env.SESSION_SECRET,
     saveUninitialized: true,
     resave: false,
     store: MongoStore.create({
-        mongoUrl: process.env.DB_URI
+      mongoUrl: process.env.DB_URI,
     }),
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24 
-    }
-}));
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
 
 //middleware to print session details of authenticated user
 app.use((req, res, next) => {
-    console.log(req.session);
-    console.log(req.user);
-    next();
+  console.log(req.session);
+  console.log(req.user);
+  next();
 });
 
+app.use(homeRoute);
+app.use(userRoute);
+app.use(passwordHintRoute);
+app.use(applicationPasswordsRoute);
 
-app.use(home_route);
-app.use(user_route);
-app.use(password_hint_route);
-app.use(application_passwords_route);
-
-
-app.listen(app.get('port'), () => {
-    console.log(`The server has started and is listening on port number: ${app.get('port')}`);
+app.listen(app.get("port"), () => {
+  console.log(
+    `The server has started and is listening on port number: ${app.get("port")}`
+  );
 });
